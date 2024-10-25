e15d17ea9cec2ff6f1f66f58db199918f76ceac5
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2024-10-25 17:14:26 +0200

    Formatting fix

--- transport-company-database/tables-ddl/people.sql
+++ transport-company-database/tables-ddl/people.sql
@@ -1,9 +1,9 @@
 create table people (
     id integer primary key generated always as identity,
-    -- The type is 'varchar(20)' just for not having
-    --  to define a check constraint with 'text' or
-    --  'varchar'. I do not understand check
-    --  constraints yet. The length '20' is a
+    -- The type is "varchar(20)" just for not having
+    --  to define a check constraint with "text" or
+    --  "varchar". I do not understand check
+    --  constraints yet. The length "20" is a
     --  more or less arbitrary value, but which
     --  seems to me to be short enough and long
     --  enough for my needs during my learning of SQL
--- transport-company-database/tables-ddl/vehicles.sql
+++ transport-company-database/tables-ddl/vehicles.sql
@@ -1,7 +1,7 @@
 create table vehicles (
     id integer primary key generated always as identity,
     max_cargo_weight_in_kg integer not null,
-    -- The type is 'varchar(20)', because I expect
+    -- The type is "varchar(20)", because I expect
     --  this column to store strings representing
     --  decimal degrees, in a form of
     --  "90.123456,180.123456".
