f19c11aefc43a27eb904b5d0d36cc6b53f6357e9
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2024-10-25 16:41:56 +0200

    Formatting fix

--- transport-company-database/tables-ddl/companies.sql
+++ transport-company-database/tables-ddl/companies.sql
@@ -1,7 +1,7 @@
 create table companies (
     id integer primary key generated always as identity,
-        -- The type is 'integer' because I think it is
-        --  simple. But in general I believe I could also
-        --  use 'varchar' or 'text'.
+    -- The type is 'integer' because I think it is
+    --  simple. But in general I believe I could also
+    --  use 'varchar' or 'text'.
     registration_number integer not null
 );
